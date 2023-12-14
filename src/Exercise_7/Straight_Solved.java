32dcaaa1b7300a983c7a31ef57bec82061a58486
Iván Delgado Terroso <ivandelter@outlook.com>
2023-12-14 11:40:11 +0100

    Not finished the last exercise solve

--- src/Exercise_7/Straight_Solved.java
+++ src/Exercise_7/Straight_Solved.java
@@ -1,4 +1,26 @@
 package Exercise_7;
+import Exercise_6.Point;
 
 public class Straight_Solved {
+
+    private float width;
+    private Point p1;
+    private Point p2;
+
+    public Straight_Solved(float width, Point p1, Point p2){
+        this.width = width;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Straight_Solved(float width, double xOne, double xTwo, double yOne, double yTwo){
+        this.width = width;
+        this.p1 = new Point();
+        this.p2 = new Point();
+        this.p1.setX(xOne);
+        this.p2.setX(xTwo);
+        this.p1.setY(yOne);
+        this.p2.setY(yTwo);
+    }
+
 }
